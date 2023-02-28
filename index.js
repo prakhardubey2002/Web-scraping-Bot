754dab73269c5f3a1ca11290c902f931c0b00e74
Prakhar Dubey <prakhardubey2002@gmail.com>
2023-02-28 23:55:04 +0530

    Setup intialised

---
+++ index.js
@@ -0,0 +1,38 @@
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
+const Nightmare = require('nightmare')
+const nightmare = Nightmare({ show: true })
+const token = process.env.TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    console.log(msg);
+    const wordex = (msg.text.replaceAll(' ', '+'))
+    console.log(wordex);
+    if (wordex) {
+
+            var b = nightmare.goto(`https://duckduckgo.com/?q=${wordex}&t=h_&ia=web`)
+            .wait('.js-about-item-abstr')
+            .evaluate(() => document.querySelector('.OgdwYG6KE2qthn9XQWFC').innerText)
+            .then(
+                (res) => {
+                    console.log("res2")
+
+                    bot.sendMessage(chatId, `HI ${msg.chat.first_name} your relavent answer is : ${res}`);
+                    
+                }
+
+            )
+            .catch(error => {
+                console.log(error);
+            }
+            )
+
+
+
+    }
+
+});
+
